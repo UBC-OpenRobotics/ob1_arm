3c63a3ed780b0dcee9e9c6642b5a1aed28e2cbbb
Yousif El-Wishahy <yel.wishahy@gmail.com>
2022-08-13 16:59:33 -0700

    completed read/write methods with json parsing in hw interface class. Added config files for joints/controllers. Added launch files. Updated readme.

--- ob1_arm_hw_interface/src/ob1_arm_hw_interface.cpp
+++ ob1_arm_hw_interface/src/ob1_arm_hw_interface.cpp
@@ -2,46 +2,91 @@
 
 // ROS parameter loading
 #include <rosparam_shortcuts/rosparam_shortcuts.h>
+#include <arduino_json/ArduinoJson-v6.19.4.h>
 
 namespace ob1_arm_hw
 {
-Ob1ArmHWInterface::Ob1ArmHWInterface(ros::NodeHandle& nh, urdf::Model* urdf_model)
-  : ros_control_boilerplate::GenericHWInterface(nh, urdf_model), name_("ob1_arm_hw_interface")
-{
-  // Load rosparams
-  ros::NodeHandle rpnh(nh_, "hardware_interface");
-//   std::size_t error = 0;
-//   error += !rosparam_shortcuts::get(name_, rpnh, "sim_control_mode", sim_control_mode_);
-//   if (error)
-//   {
-//     ROS_WARN_STREAM_NAMED(name_, "SimHWInterface now requires the following config in the yaml:");
-//     ROS_WARN_STREAM_NAMED(name_, "   sim_control_mode: 0 # 0: position, 1: velocity");
-//   }
-//   rosparam_shortcuts::shutdownIfError(name_, error);
-}
+    Ob1ArmHWInterface::Ob1ArmHWInterface(ros::NodeHandle& nh, urdf::Model* urdf_model)
+    : ros_control_boilerplate::GenericHWInterface(nh, urdf_model), name_("ob1_arm_hw_interface")
+    {
+        // Load rosparams
+        ros::NodeHandle rpnh(nh_, "hardware_interface");
+    }
 
-void Ob1ArmHWInterface::init()
-{
-  // Call parent class version of this function
-  GenericHWInterface::init();
+    void Ob1ArmHWInterface::init()
+    {
+        // Call parent class version of this function
+        GenericHWInterface::init();
+        msgCount_ = 0;
 
-  ROS_INFO_NAMED(name_, "Ob1ArmHWInterface Ready.");
-}
+        //need 2 serial ports for comms
+        arduinoOutput.open("/dev/ttyACM0"); //output serial port to write to
+        arduinoInput.open("/dev/ttyACM0"); //input serial port to read from
+        ROS_INFO_NAMED(name_, "Ob1ArmHWInterface: Opened serial port streams");
 
-void Ob1ArmHWInterface::read(ros::Duration& elapsed_time)
-{
-  // No need to read since our write() command populates our state for us
-}
+        ROS_INFO_NAMED(name_, "Ob1ArmHWInterface Ready.");
+    }
 
-void Ob1ArmHWInterface::write(ros::Duration& elapsed_time)
-{
+    void Ob1ArmHWInterface::close_serial()
+    {
+        arduinoOutput.close();
+        arduinoInput.close();
+        ROS_INFO_NAMED(name_, "Ob1ArmHWInterface: Closed serial port streams");
+    }
 
-}
+    void Ob1ArmHWInterface::read(ros::Duration& elapsed_time)
+    {
+        /** Need to populate the following data for robot state
+         *   // States
+            std::vector<double> joint_position_;
+            std::vector<double> joint_velocity_;
+        */
+       StaticJsonDocument<256> jsonMsg;
+       DeserializationError err = deserializeJson(jsonMsg, arduinoInput);
 
-void Ob1ArmHWInterface::enforceLimits(ros::Duration& period)
-{
-  // Enforces position and velocity
-  pos_jnt_sat_interface_.enforceLimits(period);
-}
+       if(err == DeserializationError::Ok)
+       {
+            for(int i = 0; i < num_joints_; i++){
+                joint_position_[i] = jsonMsg["angle"][i];
+                joint_velocity_[i] = jsonMsg["vel"][i];
+            }
+       }
+
+    }
+
+    void Ob1ArmHWInterface::write(ros::Duration& elapsed_time)
+    {
+        /**
+         angle, vel and effort commands for joints 
+         are stored and updated in these variables internally.
+
+         std::vector<double> joint_position_command_;
+         std::vector<double> joint_velocity_command_;
+        */
+
+        //see readme for example message
+        //get json file size with tool: https://arduinojson.org/v6/assistant/
+
+        StaticJsonDocument<256> jsonMsg;
+
+        //populate json msg
+        //TODO: radians -> degree conversion if neccessary
+        // does robot accept radians or degrees? 
+        jsonMsg["msg_counter"] = msgCount_;
+        jsonMsg["num_joints"] = num_joints_;
+        for(int i = 0; i < num_joints_; i++){
+            jsonMsg["angle"][i] = joint_position_command_[i];
+            jsonMsg["vel"][i] = joint_velocity_command_[i];
+        }
+
+        //write to serial port stream
+        serializeJson(jsonMsg, arduinoOutput);
+    }
+
+    void Ob1ArmHWInterface::enforceLimits(ros::Duration& period)
+    {
+        // Enforces position and velocity
+        pos_jnt_sat_interface_.enforceLimits(period);
+    }
 
 }  // namespace ros_control_boilerplate
--- ob1_arm_hw_interface/src/ob1_arm_hw_main.cpp
+++ ob1_arm_hw_interface/src/ob1_arm_hw_main.cpp
@@ -3,22 +3,23 @@
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "sim_hw_interface");
-  ros::NodeHandle nh;
+    ros::init(argc, argv, "sim_hw_interface");
+    ros::NodeHandle nh;
 
-  // NOTE: We run the ROS loop in a separate thread as external calls such
-  // as service callbacks to load controllers can block the (main) control loop
-  ros::AsyncSpinner spinner(3);
-  spinner.start();
+    // NOTE: We run the ROS loop in a separate thread as external calls such
+    // as service callbacks to load controllers can block the (main) control loop
+    ros::AsyncSpinner spinner(3);
+    spinner.start();
 
-  // Create the hardware interface specific to your robot
-  std::shared_ptr<ob1_arm_hw::Ob1ArmHWInterface> ob1_arm_hw_interface(
-      new ob1_arm_hw::Ob1ArmHWInterface(nh));
-  ob1_arm_hw_interface->init();
+    // Create the hardware interface specific to your robot
+    std::shared_ptr<ob1_arm_hw::Ob1ArmHWInterface> ob1_arm_hw_interface(
+        new ob1_arm_hw::Ob1ArmHWInterface(nh));
+    ob1_arm_hw_interface->init();
 
-  // Start the control loop
-  ros_control_boilerplate::GenericHWControlLoop control_loop(nh, ob1_arm_hw_interface);
-  control_loop.run();  // Blocks until shutdown signal recieved
+    // Start the control loop
+    ros_control_boilerplate::GenericHWControlLoop control_loop(nh, ob1_arm_hw_interface);
+    control_loop.run();  // Blocks until shutdown signal recieved
 
-  return 0;
+    ob1_arm_hw_interface->close_serial();
+    return 0;
 }
