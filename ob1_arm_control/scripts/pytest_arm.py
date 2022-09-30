64e7cd91512851ac4c04ed9f60595a2e406cf12d
Yousif El-Wishahy <yel.wishahy@gmail.com>
2022-09-30 16:57:18 -0700

    pytest test cases for motion planning. also modified collisions for moveitconfig

---
+++ ob1_arm_control/scripts/pytest_arm.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python3.8
+from __future__ import print_function
+from operator import itemgetter
+import pickle
+import time
+from arm_commander import ArmCommander, convert_to_list
+import sys
+import rospy
+import moveit_commander
+from moveit_commander.conversions import pose_to_list
+import numpy as np
+from math import pi, tau, dist, fabs, cos
+import geometry_msgs
+from geometry_msgs.msg import PoseStamped, Pose, Vector3Stamped 
+from moveit_commander import MoveGroupCommander,RobotCommander, PlanningSceneInterface
+from moveit_commander.planning_scene_interface import CollisionObject
+from moveit_msgs.msg import Grasp, GripperTranslation, MoveItErrorCodes,DisplayTrajectory
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from tf.transformations import quaternion_from_euler
+from copy import deepcopy
+import rospkg
+import pytest
+import logging
+
+##TEST PARAMS
+# rp = rospkg.RosPack()
+# PACKAGE_PATH = rp.get_path('ob1_arm_control')
+# DATA_FILE_PATH = PACKAGE_PATH+"/data/5k_pose_data.pickle"
+
+PLANNING_TOLERANCE = 0.05 #distance in metres
+SUCCESS_RATE_TOLERANCE = 0.80 #percentage of tests that pass
+
+iterations = 10
+
+log = logging.getLogger(__name__)
+
+arm_commander = ArmCommander(sample_time_out=5,goal_tolerance=0.01)
+log.info("Clearing scene..")
+arm_commander.scene.clear()
+
+def dist(p1,p2):
+    p1 = np.array(convert_to_list(p1))
+    p2 = np.array(convert_to_list(p2))
+    return np.linalg.norm(p1-p2)
+
+def assert_ik_result(current_state,target,tolerance:float):
+    if type(target) is PoseStamped and type(current_state) is PoseStamped:
+        assert target.header.frame_id == current_state.header.frame_id , "transform frame mismatch"
+    d = dist(current_state,target)
+    assert d <= tolerance, "Distance from target is too far, %f cm" % (d*100)
+
+def compare_ik_result(current_state,target,tolerance:float):
+    if type(target) is PoseStamped and type(current_state) is PoseStamped:
+        if target.header.frame_id != current_state.header.frame_id:
+            return False, "transform frame mismatch"
+    d = dist(current_state,target)
+    if d <= tolerance:
+        return True, "Motion planning tolerance success"
+    else:
+        return False, "Distance from target is too far, %f cm" % (d*100)
+
+def test_go_rand_pose():
+    log.info("Starting go_rand_pose test")
+    log.info("Going to random pose...")
+    res, target = arm_commander.go_pose()
+    assert res , "motion planning failed"
+    log.info("Boolean result: %s" % res)
+    log.info("Target type: %s" % type(target))
+    assert_ik_result(arm_commander.get_end_effector_pose(),target,PLANNING_TOLERANCE)
+
+def test_go_rand_position():
+    log.info("Starting go_rand_position test")
+    log.info("Going to random position...")
+    res, target = arm_commander.go_position()
+    assert res , "motion planning failed"
+    log.info("Boolean result: %s" % res)
+    log.info("Target type: %s" % type(target))
+    assert_ik_result(arm_commander.get_end_effector_pose(),target,PLANNING_TOLERANCE)
+
+def test_go_rand_joint_target():
+    log.info("Starting go_rand_joint_target test")
+    log.info("Going to random joint target...")
+    res, target = arm_commander.go_joint()
+    assert res , "motion planning failed"
+    log.info("Boolean result: %s" % res)
+    log.info("Target type: %s" % type(target))
+    assert_ik_result(arm_commander.arm_mvgroup.get_current_joint_values(),target,PLANNING_TOLERANCE)
+
+def test_go_rand_ikpoint():
+    log.info("Starting go_rand_ikpoint test")
+    log.info("Generating random point and matching nearest ikpoint...")
+    res, target, joint_target = arm_commander.go_position_ikpoints()
+    assert res , "motion planning failed"
+    log.info("Boolean result: %s" % res)
+    log.info("Target type: %s" % type(target))
+    assert_ik_result(arm_commander.get_end_effector_pose(),target,PLANNING_TOLERANCE)
+
+def test_go_rand_reachable_scene_object():
+    log.info("Starting go_rand_reachable_scene_object test")
+
+    log.info("Clearing scene..")
+    arm_commander.scene.clear()
+
+    log.info("Generating and placing scene object..")
+    p = PoseStamped()
+    p.header.frame_id = arm_commander.robot.get_planning_frame()
+    rand_pos = arm_commander.ikpoints.get_rand_point()
+    p.pose.position.x = rand_pos[0]
+    p.pose.position.y = rand_pos[1]
+    p.pose.position.z = rand_pos[2]
+    p.pose.orientation.w = 1
+    arm_commander.scene.add_sphere("sphere",p,0.1)
+
+    log.info("Detecting scene objects..")
+    objs = arm_commander.scene.get_objects()
+    assert len(objs) > 0, "Could not find any scene objects"
+    obj:CollisionObject = list(objs.items())[0][1]
+    pos = [obj.pose.position.x,obj.pose.position.y,obj.pose.position.z]
+
+    log.info("Motion planning to scene object")
+    res, target, joint_target = arm_commander.go_position_ikpoints(pos)
+
+    assert res , "motion planning failed"
+
+    log.info("Boolean result: %s" % res)
+    log.info("Target type: %s" % type(target))
+    assert_ik_result(arm_commander.get_end_effector_pose(),target,PLANNING_TOLERANCE)
+
+    log.info("Clearing scene..")
+    arm_commander.scene.clear()
+
+def test_go_rand_scene_object_smart():
+    log.info("Starting go_rand_scene_object test")
+
+    log.info("Clearing scene..")
+    arm_commander.scene.clear()
+
+    log.info("Generating and placing scene object..")
+    p = PoseStamped()
+    p.header.frame_id = arm_commander.robot.get_planning_frame()
+    rand_pos = arm_commander.ikpoints.get_rand_point()
+    p.pose.position.x = rand_pos[0]
+    p.pose.position.y = rand_pos[1]
+    p.pose.position.z = rand_pos[2]
+    p.pose.orientation.w = 1
+    arm_commander.scene.add_sphere("sphere",p,0.1)
+
+    log.info("Detecting scene objects..")
+    objs = arm_commander.scene.get_objects()
+
+    assert len(objs) > 0, "Could not find any scene objects"
+
+    obj:CollisionObject = list(objs.items())[0][1]
+    p1 = np.ndarray([obj.pose.position.x,obj.pose.position.y,obj.pose.position.z])
+
+    log.info("Motion planning to scene object")
+
+    res, target, joint_target = arm_commander.go_position_ikpoints(p1)
+    if not res:
+        log.info("Motion planning object failed.. trying to plan further away")
+        dx = 0.1
+        for i in range(iterations):
+            p2 = p1 * (1 - dx*i)
+            log.info("scaled point by %f" %(1-dx*i))
+            res, target, joint_target = arm_commander.go_position_ikpoints(p2)
+            if res:
+                break
+    assert res , "motion planning failed"
+
+    log.info("Boolean result: %s" % res)
+    log.info("Target type: %s" % type(target))
+    assert_ik_result(arm_commander.get_end_effector_pose(),target,PLANNING_TOLERANCE)
+
+    log.info("Clearing scene..")
+    arm_commander.scene.clear()
+
+def test_go_rand_pose_loop():
+    success_counter = 0
+    for _ in range(iterations):
+        res, target = arm_commander.go_pose()
+        compare_res, msg = compare_ik_result(arm_commander.get_end_effector_pose(),target,PLANNING_TOLERANCE)
+        if res and compare_res:
+            success_counter+=1
+    
+    assert success_counter/10 >= SUCCESS_RATE_TOLERANCE, "Motion planning success rate is too low. %f" %(success_counter/10)
+
+def test_go_rand_ikpoint_loop():
+    success_counter = 0
+    for _ in range(iterations):
+        res, target, joint_target = arm_commander.go_position_ikpoints()
+        compare_res, msg = compare_ik_result(arm_commander.get_end_effector_pose(),target,PLANNING_TOLERANCE)
+        if res and compare_res:
+            success_counter+=1
+    
+    assert success_counter/10 >= SUCCESS_RATE_TOLERANCE, "Motion planning success rate is too low. %f" %(success_counter/10)
+
+def test_avg_go_ikpoint_dist():
+    d = 0
+    for _ in range(iterations):
+        res, target, joint_target = arm_commander.go_position_ikpoints()
+        if res:
+            d+=dist(target,arm_commander.get_end_effector_pose())
+    d_avg = d/iterations
+
+    assert d_avg <= PLANNING_TOLERANCE, "Avg ik point distance too far: %s cm" %(d_avg*100)
+        
+
+
+    
--- ob1_arm_control/scripts/test_arm.py
+++ ob1_arm_control/scripts/test_arm.py
@@ -2,7 +2,7 @@
 from __future__ import print_function
 import pickle
 import time
-from arm_commander import ArmCommander
+from arm_commander import ArmCommander, assert_ik_result
 import sys
 import rospy
 import moveit_commander
@@ -17,196 +17,24 @@ from moveit_msgs.msg import Grasp, GripperTranslation, MoveItErrorCodes,DisplayT
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from tf.transformations import quaternion_from_euler
 from copy import deepcopy
-from distutils.util import rfc822_escape
 import rospkg
+import pytest
 
+##TEST PARAMS
 rp = rospkg.RosPack()
 PACKAGE_PATH = rp.get_path('ob1_arm_control')
 DATA_FILE_PATH = PACKAGE_PATH+"/data/5k_pose_data.pickle"
 
 HOR = '-'*20
 
-############################
-# Arm and gripper grasp object test
-# the following code is adapated from 
-# https://github.com/AIWintermuteAI/ros-moveit-arm/blob/master/my_arm_xacro/pick/pick.py
-# with some minor changes
-###########################
-
-def degrees_to_radians(joint_angles):
-    for i in range(len(joint_angles)):
-        joint_angles[i] = joint_angles[i] * np.pi/180.0
-    return joint_angles
-
-
-# Get the gripper posture as a JointTrajectory
-def make_gripper_posture(arm_commander : ArmCommander, joint_positions):
-    # Initialize the joint trajectory for the gripper joints
-    t = JointTrajectory()
-    
-    # Set the joint names to the gripper joint names
-    t.joint_names = arm_commander.robot.get_joint_names(arm_commander.gripper_mvgroup.get_name())
-    
-    # Initialize a joint trajectory point to represent the goal
-    tp = JointTrajectoryPoint()
-    
-    # Assign the trajectory joint positions to the input positions
-    tp.positions = joint_positions
-    
-    # Set the gripper effort
-    tp.effort = [1.0]
-    
-    tp.time_from_start = rospy.Duration(1.0)
-    
-    # Append the goal point to the trajectory points
-    t.points.append(tp)
-    
-    # Return the joint trajectory
-    return t
-
-# Generate a gripper translation in the direction given by vector
-def make_gripper_translation(arm_commander :ArmCommander, min_dist, desired, vector):
-    # Initialize the gripper translation object
-    g = GripperTranslation()
-    
-    # Set the direction vector components to the input
-    g.direction.vector.x = vector[0]
-    g.direction.vector.y = vector[1]
-    g.direction.vector.z = vector[2]
-    
-    # The vector is relative to the gripper frame
-    g.direction.header.frame_id = arm_commander.ARM_REF_FRAME
-    
-    # Assign the min and desired distances from the input
-    g.min_distance = min_dist
-    g.desired_distance = desired
-    
-    return g
-
-# Generate a list of possible grasps
-def make_grasps(arm_commander, initial_pose_stamped, allowed_touch_objects):
-    gripper_open_joints = degrees_to_radians([-20.0, 0.0, -80.0, 0.0, -80.0])
-    gripper_close_joints = degrees_to_radians([-15.0, 0.0, -2.0, 0.0, -2.0])
-    arm_pos_joints = degrees_to_radians([-4.0, -29.0, -68.0, -180.0, 83.0])
-
-    # Initialize the grasp object
-    g = Grasp()
-    
-    # Set the pre-grasp and grasp postures appropriately
-    g.pre_grasp_posture = make_gripper_posture(arm_commander, gripper_open_joints)
-    g.grasp_posture = make_gripper_posture(arm_commander, gripper_close_joints)
-
-    # Set the approach and retreat parameters as desired
-    g.pre_grasp_approach = make_gripper_translation(arm_commander, 0.001, 0.001, [0, 0, -1])
-    g.post_grasp_retreat = make_gripper_translation(arm_commander, 0.1, 0.15, [0, 0, 1])
-    
-    # Set the first grasp pose to the input pose
-    g.grasp_pose = initial_pose_stamped
-
-    # A list to hold the grasps
-    grasps = []
-    grasps.append(deepcopy(g))
-
-    ideal_roll = 0
-    ideal_pitch = 0
-    ideal_yaw = 0
-    
-    step_size = 0.1
-    idx = 0.1
-    idx_roll = ideal_roll + idx
-    idx_pitch = ideal_pitch + idx
-    idx_yaw = ideal_yaw + idx
-    roll_vals = []
-    pitch_vals = []
-    yaw_vals = []
-    while idx >= -0.1:
-        roll_vals.append(idx_roll)
-        pitch_vals.append(idx_pitch)
-        yaw_vals.append(idx_yaw)
-        idx -= step_size
-        idx_roll -= step_size
-        idx_pitch -= step_size
-        idx_yaw -= step_size
-        
-        # Generate a grasp for each roll pitch and yaw angle
-        for r in roll_vals:
-            for y in yaw_vals:
-                for p in pitch_vals:
-                    # Create a quaternion from the Euler angles
-                    q = quaternion_from_euler(r, p, y)
-                    
-                    # Set the grasp pose orientation accordingly
-                    g.grasp_pose.pose.orientation.x = q[0]
-                    g.grasp_pose.pose.orientation.y = q[1]
-                    g.grasp_pose.pose.orientation.z = q[2]
-                    g.grasp_pose.pose.orientation.w = q[3]
-                    
-                    # Set and id for this grasp (simply needs to be unique)
-                    g.id = str(len(grasps))
-                    
-                    # Set the allowed touch objects to the input list
-                    g.allowed_touch_objects = allowed_touch_objects
-                    
-                    # Don't restrict contact force
-                    g.max_contact_force = 0
-                    
-                    # Degrade grasp quality for increasing pitch angles
-                    g.grasp_quality = 1.0 - abs(p)
-                    
-                    # Append the grasp to the list
-                    grasps.append(deepcopy(g))
-                    print(g)
-                    
-        print("Generated " + str(len(grasps)) + " poses")
-        # Return the list
-        return grasps
-
-def grasp_test(arm_commander: ArmCommander,max_attempts=25):
-    #clean scene
-    arm_commander.scene.remove_world_object("part")
-    arm_commander.scene.remove_world_object("table")
-
-    #place scene objects for test
-    obj_pos = PoseStamped()
-    obj_pos.header.frame_id = arm_commander.ARM_REF_FRAME #todo: test/change reference frame for these objects
-
-    obj_pos.pose.orientation.w = 1.0 #leave other quat. values at default
-
-    obj_pos.pose.position.x = 0.0
-    obj_pos.pose.position.y = 0.46
-    obj_pos.pose.position.z = 0.025
-
-    table_size = (0.5,0.5,0.05)
-    arm_commander.scene.add_box("table", obj_pos, table_size)
-    arm_commander.arm_mvgroup.set_support_surface_name("table") #for place operation
-
-    obj_pos.pose.position.x = 0.01
-    obj_pos.pose.position.y = 0.29
-    obj_pos.pose.position.z = 0.1
-    arm_commander.scene.add_cylinder("part", obj_pos, 0.2, 0.04)
-
-
-    #generate a list of grasp approaches from current eef pose
-    #initial pose target
-    init_pose = PoseStamped()
-    init_pose = arm_commander.get_end_effector_pose()
-    grasps = make_grasps(arm_commander, init_pose, ['part'])
-
-    #attempt pick operation and report success/failure
-    result = None
-    n_attempts = 0
-    while result != MoveItErrorCodes.SUCCESS and n_attempts < max_attempts:
-        n_attempts += 1
-        rospy.loginfo("Pick attempt: " +  str(n_attempts))
-        result = arm_commander.arm_mvgroup.pick('part',grasps)
-        rospy.sleep(0.2)
+TEST_TOLERANCE = 0.05
 
 def go_pose_test(arm_commander: ArmCommander,iterations):
     """go to random poses generated within reach and test end effector pose accuracy after ik motion complete"""
     success_counter = 0
     counter = 0
     for _ in range(iterations):
-        arm_commander.go_pose()
+        res, target = arm_commander.go_pose()
         # print(arm_commander.pose_goal)
         # print(arm_commander.get_end_effector_pose())
         if arm_commander.compare_eef_pose_states():
